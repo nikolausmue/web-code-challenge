cfa66486d04c35dde91de693b0aaf7758c4d2c68
Nikolaus Müller <nikolaus@waytation.com>
2018-03-20 21:52:37 +0100

    implemented  modal, implemented validation

--- beerd-app/src/app/app.module.ts
+++ beerd-app/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { BeerFormComponent } from './beer-form/beer-form.component';
-
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,9 @@ import { BeerFormComponent } from './beer-form/beer-form.component';
     BeerFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
